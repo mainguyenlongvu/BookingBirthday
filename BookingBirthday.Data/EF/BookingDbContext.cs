ed3955f714c81579d3e1de0ae70e0a27e13e51b9
0969680078 <mainguyenlongvu0703@gmail.com>
2024-01-24 21:53:36 +0700

    Add project files.

---
+++ BookingBirthday.Data/EF/BookingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookingBirthday.Data.EF
+{
+    public class BookingDBContext : DbContext
+    {
+        public BookingDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+
+
+}
