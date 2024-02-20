bbcc0b31471c70c42d56f977384416ea2f4823b5
0969680078 <mainguyenlongvu0703@gmail.com>
2024-02-20 22:23:01 +0700

    add  BookingBirthday.Server, delete web api because it have a lot of BUG :)

---
+++ BookingBirthday.Application/Helper/ApplicationMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BookingBirthday.Data.Entities;
+using BookingBirthday.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingBirthday.Application.Helper
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Package, PackageModel>().ReverseMap();
+        }
+    }
+}
