59f876ae66f5e71be4e0abdc943bcd4a1831d040
0969680078 <mainguyenlongvu0703@gmail.com>
2024-03-15 21:10:51 +0700

    update

---
+++ BookingBirthday.Server/Common/CreateMD5Short.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BookingBirthday.Server.Common
+{
+    public class CreateMD5Short
+    {
+        public static string MD5Short(string input)
+        {
+            // Use input string to calculate MD5 hash
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Convert the byte array to a shorter hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 4; i++) // Chỉ lấy 4 byte đầu tiên
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
