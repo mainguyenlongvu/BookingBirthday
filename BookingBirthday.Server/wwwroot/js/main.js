381acc0c7676eb3b40300ee2cc338aa7f8703282
nhatduy111003 <duypnse173520@fpt.edu.vn>
2024-03-16 17:22:09 +0700

    Update FE

--- BookingBirthday.Server/wwwroot/js/main.js
+++ BookingBirthday.Server/wwwroot/js/main.js
@@ -16,22 +16,22 @@
     new WOW().init();
 
 
-    // Fixed Navbar
-    $(window).scroll(function () {
-        if ($(window).width() < 992) {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow');
-            } else {
-                $('.fixed-top').removeClass('bg-white shadow');
-            }
-        } else {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow').css('top', -45);
-            } else {
-                $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
-            }
-        }
-    });
+    //// Fixed Navbar
+    //$(window).scroll(function () {
+    //    if ($(window).width() < 992) {
+    //        if ($(this).scrollTop() > 45) {
+    //            $('.fixed-top').addClass('bg-white shadow');
+    //        } else {
+    //            $('.fixed-top').removeClass('bg-white shadow');
+    //        }
+    //    } else {
+    //        if ($(this).scrollTop() > 45) {
+    //            $('.fixed-top').addClass('bg-white shadow').css('top', -45);
+    //        } else {
+    //            $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
+    //        }
+    //    }
+    //});
 
 
     // Back to top button
