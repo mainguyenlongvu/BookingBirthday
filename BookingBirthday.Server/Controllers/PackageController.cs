39176661cca69c941ea0e9f2a19afd6dc97bb3d8
truongdaivy57 <truongdaivy57@gmail.com>
2024-03-22 15:41:20 +0700

    Update

--- BookingBirthday.Server/Controllers/PackageController.cs
+++ BookingBirthday.Server/Controllers/PackageController.cs
@@ -18,72 +18,6 @@ namespace BookingBirthday.Server.Controllers
         {
             _dbContext = dbContext;
         }
-        //public IActionResult Show(int Id)
-        //{
-        //    var session = HttpContext.Session;
-        //    var role = HttpContext.Session.GetString("role");
-        //    List<Category_requests> category_request = null!;
-
-        //    if (role == "Admin")
-        //    {
-        //        category_request = _dbContext.Category_Requests
-        //            .Where(x => x.is_approved == 0 && x.is_deleted_by_admin == false)
-        //            .OrderByDescending(x => x.created_at)
-        //            .ToList();
-        //    }
-        //    else if (role == "Host")
-        //    {
-        //        var user_id = int.Parse(HttpContext.Session.GetString("user_id")!);
-        //        category_request = _dbContext.Category_Requests
-        //            .Where(x => x.is_deleted_by_owner == false && x.requester_id == user_id && (x.is_approved == -1 || x.is_approved == 1))
-        //            .OrderByDescending(x => x.created_at)
-        //            .ToList();
-        //    }
-
-        //    if (category_request != null)
-        //    {
-        //        var jsonNotification = JsonConvert.SerializeObject(category_request);
-        //        session.SetString("notification", jsonNotification);
-        //    }
-
-        //    //var package = (from a in _dbContext.Packages
-        //    //               join b in _dbContext.Users on a.UserId equals b.Id
-        //    //               where a.Id == Id && b.Status == "Active"
-        //    //               select new PackageModel
-        //    //               {
-        //    //                   Id = a.Id,
-        //    //                   Name = a.Name,
-        //    //                   Venue = a.Venue,
-        //    //                   Detail = a.Detail,
-        //    //                   Note = a.Note,
-        //    //                   Price = a.Price,
-        //    //                   image_url = a.image_url
-        //    //               }).FirstOrDefault();
-
-        //    //if (package != null)
-        //    //{
-        //    //    return View(package);
-        //    //}
-        //    //return null;
-
-        //    var product = _dbContext.Packages.Include(x => x.Category).Where(x => x.Id == Id).FirstOrDefault();
-        //    if (product != null)
-        //    {
-        //        var p = new PackageModel();
-        //        p.Id = product.Id;
-        //        p.Name = product.Name;
-        //        p.Venue = product.Venue;
-        //        p.Detail = product.Detail;
-        //        p.Note = product.Note;
-        //        p.Price = product.Price;
-        //        p.image_url = product.image_url;
-        //        p.Status = product.Status;
-        //        p.Host_name = product.Host_name;
-        //        p.UserId = product.UserId;
-        //        return View(p);
-        //    }
-        //    return View(product);
-        //}
 
         public IActionResult ProfileHost(int UserId)
         {
@@ -151,6 +85,50 @@ namespace BookingBirthday.Server.Controllers
 
                 ViewBag.AverageRate = avgRate;
             }
+
+            var session = HttpContext.Session;
+            var role = HttpContext.Session.GetString("role");
+            List<Category_requests> category_request = null!;
+
+            if (role == "Admin")
+            {
+                category_request = _dbContext.Category_Requests
+                    .Where(x => x.is_approved == 0 && x.is_deleted_by_admin == false)
+                    .OrderByDescending(x => x.created_at)
+                    .ToList();
+            }
+            else if (role == "Host")
+            {
+                var user_id = int.Parse(HttpContext.Session.GetString("user_id")!);
+                category_request = _dbContext.Category_Requests
+                    .Where(x => x.is_deleted_by_owner == false && x.requester_id == user_id && (x.is_approved == -1 || x.is_approved == 1))
+                    .OrderByDescending(x => x.created_at)
+                    .ToList();
+            }
+
+            if (category_request != null)
+            {
+                var jsonNotification = JsonConvert.SerializeObject(category_request);
+                session.SetString("notification", jsonNotification);
+            }
+
+            package = _dbContext.Packages.Include(x => x.Category).Where(x => x.Id == id).FirstOrDefault();
+            if (package != null)
+            {
+                var p = new PackageModel();
+                p.Id = package.Id;
+                p.Name = package.Name;
+                p.Venue = package.Venue;
+                p.Detail = package.Detail;
+                p.Note = package.Note;
+                p.Price = package.Price;
+                p.image_url = package.image_url;
+                p.Status = package.Status;
+                p.Host_name = package.Host_name;
+                p.UserId = package.UserId;
+                return View(p);
+            }
+
             return View(package);
         }
 
