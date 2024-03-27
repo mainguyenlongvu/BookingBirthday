7768b0276b7f309da05887497f6e40b73f8457e9
truongdaivy57 <truongdaivy57@gmail.com>
2024-03-27 12:10:35 +0700

    Update Reste Password

--- BookingBirthday.Server/Models/ResetPasswordModel.cs
+++ BookingBirthday.Server/Models/ResetPasswordModel.cs
@@ -5,6 +5,7 @@ namespace BookingBirthday.Server.Models
     public class ResetPasswordModel
     {
         [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới", AllowEmptyStrings = false)]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Mật khẩu mới phải từ 8 đến 30 ký tự")]
         [DataType(DataType.Password)]
         public string NewPassword { get; set; }
 
@@ -12,7 +13,7 @@ namespace BookingBirthday.Server.Models
         [Compare("NewPassword", ErrorMessage = "Mật khẩu xác nhận không trùng khớp")]
         public string ConfirmPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Mã đặt lại là bắt buộc")]
         public string ResetCode { get; set; }
     }
 }
