1b3ed54111db6e45720c4a5d62f572f524dbf61e
truongdaivy57 <truongdaivy57@gmail.com>
2024-04-04 18:28:53 +0700

    Update

--- BookingBirthday.Server/Models/PackageModel.cs
+++ BookingBirthday.Server/Models/PackageModel.cs
@@ -28,5 +28,6 @@ namespace BookingBirthday.Server.Models
         public List<PackageLocation> PackageLocations { get; set; }
         public List<string> LocationNames { get; set; }
         public List<string> LocationAddresses { get; set; }
+        public List<Location> Locations {  get; set; }
     }
 }
